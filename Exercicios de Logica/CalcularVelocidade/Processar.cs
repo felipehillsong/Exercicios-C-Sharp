99ddd92e37055b135174ff1a594f8937467dc4fb
Felipe Silva <feliperochasilva1987@gmail.com>
2019-06-13 17:23:23 -0300

    new folder of logical exercicises

---
+++ Exercicios de Logica/CalcularVelocidade/Processar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularVelocidade
+{
+    public class Processar
+    {
+        public void Processamento()
+        {
+            ReceberVelocidade calculo = new ReceberVelocidade();
+
+            Console.WriteLine("Entre com uma velocidade: ");
+            double velocidade = double.Parse(Console.ReadLine());
+
+            calculo.PegarVelocidade(velocidade);
+
+            calculo.CalculandoVelocidade();           
+
+        }     
+
+    }
+}
---
+++ Exercicios de Logica/CalcularVelocidade/ReceberVelocidade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularVelocidade
+{
+    public class ReceberVelocidade
+    {
+        private double Velociade { get; set; }
+
+        public double PegarVelocidade(double velocidade)
+        {
+            this.Velociade = velocidade;
+
+            return this.Velociade;
+        }
+
+        public void CalculandoVelocidade()
+        {
+            this.Velociade *= 3.6;
+            Console.WriteLine("A velocidade em metros por segundo é: " + this.Velociade);
+        }
+    }
+}
