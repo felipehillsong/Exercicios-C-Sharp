eb9303fdf93760bc7a33b3688d4af355ba3420b0
Felipe Silva <feliperochasilva1987@gmail.com>
2019-06-17 01:05:47 -0300

    modifield crud_simplificado

--- CRUD_SIMPLIFICADO/App_Start/RouteConfig.cs
+++ CRUD_SIMPLIFICADO/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CRUD_SIMPLIFICADO
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "PessoaController", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- CRUD_SIMPLIFICADO/Models/Repositorio.cs
+++ CRUD_SIMPLIFICADO/Models/Repositorio.cs
@@ -1,104 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
+using System.Data;
+using System.Configuration;
 
 namespace CRUD_SIMPLIFICADO.Models
 {
-    public class Repositorio : IDisposable
+    public class Repositorio
     {
-        private SqlConnection connection;
-
-        public Repositorio()
-        {
-            string strConn = "Data Source=localhost;Initial Catalog=Cadastro;Integrated Security=true";
-            connection = new SqlConnection(strConn);
-            connection.Open();
-        }
-
-        public void Dispose()
-        {
-            connection.Close();
-        }
-
-        public void Create(Pessoa pessoa)
-        {
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = connection;
-            cmd.CommandText = @"INSERT INTO usuarios VALUES (@nome, @cpf, @rg, @telefoneFixo, @telefoneCelular, @endereco, @email, @observacoes)";
-
-            cmd.Parameters.AddWithValue("@nome", pessoa.Nome);
-            cmd.Parameters.AddWithValue("@email", pessoa.CPF);
-            cmd.Parameters.AddWithValue("@email", pessoa.RG);
-            cmd.Parameters.AddWithValue("@email", pessoa.TelefoneFixo);
-            cmd.Parameters.AddWithValue("@email", pessoa.TelefoneCelular);
-            cmd.Parameters.AddWithValue("@email", pessoa.Email);
-            cmd.Parameters.AddWithValue("@email", pessoa.Observacoes);
-
-
-            cmd.ExecuteNonQuery();
-        }
-
-        public List<Pessoa> Read()
-        {
-
-            List<Pessoa> lista = new List<Pessoa>();
-
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = connection;
-            cmd.CommandText = @"SELECT * FROM usuarios";
-
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            while (reader.Read())
-            {
-
-                Pessoa pessoa = new Pessoa();
-                pessoa.Id = (int)reader["Id"];
-                pessoa.Nome = (string)reader["Nome"];
-                pessoa.CPF = (int)reader["CPF"];
-                pessoa.RG = (int)reader["RG"];
-                pessoa.TelefoneFixo = (int)reader["TelefoneFixo"];
-                pessoa.TelefoneCelular = (int)reader["TelefoneCelular"];
-                pessoa.Endereco = (string)reader["Endereco"];
-                pessoa.Email = (string)reader["Email"];
-                pessoa.Observacoes = (string)reader["Observacoes"];
-
-                lista.Add(pessoa);
+        protected SqlConnection ConexaoDados {
+            get {
+                return new SqlConnection(
+ConfigurationManager.ConnectionStrings["ConexaoConnectionString"].ConnectionString);
             }
-
-            return lista;
         }
 
-        public void Update(Pessoa pessoa)
+        protected void ExecuteNonQuery(string sql, List<SqlParameter> parametros)
         {
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = connection;
-            cmd.CommandText = @"UPDATE usuario SET Nome=@nome, CPF=@cpf, @RG=rg, @TelefoneFixo=telefoneFixo, @TelefoneCelular=telefoneCelular, @Endreco=endereco, @Email=email, @Observacoes=observacoes WHERE IdContato=@id";
-
-            cmd.Parameters.AddWithValue("@nome", pessoa.Nome);
-            cmd.Parameters.AddWithValue("@cpf", pessoa.CPF);
-            cmd.Parameters.AddWithValue("@rg", pessoa.RG);
-            cmd.Parameters.AddWithValue("@telefoneFixo", pessoa.TelefoneFixo);
-            cmd.Parameters.AddWithValue("@telefoneCelular", pessoa.TelefoneCelular);
-            cmd.Parameters.AddWithValue("@endereco", pessoa.Endereco);
-            cmd.Parameters.AddWithValue("@email", pessoa.Email);
-            cmd.Parameters.AddWithValue("@observacoes", pessoa.Observacoes);
-            cmd.Parameters.AddWithValue("@id", pessoa.Id);
-
-            cmd.ExecuteNonQuery();
-        }
-
-        public void Delete(int id)
-        {
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = connection;
-            cmd.CommandText = @"DELETE FROM usuarios WHERE Id=@id";
-
-            cmd.Parameters.AddWithValue("@id", id);
-
-            cmd.ExecuteNonQuery();
+            using (var conexao = ConexaoDados)
+            {
+                conexao.Open();
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandType = CommandType.Text;
+                    comando.CommandText = sqlCommand;
+                    foreach (var parametro in parametros)
+                    {
+                        comando.Parameters.Add(parametro);
+                    }
+                    comando.ExecuteNonQuery();
+                }
+                conexao.Close();
+                
+            }
         }
     }
 }
