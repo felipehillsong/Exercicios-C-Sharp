e53c09474b26b3a69fb285513eff7397c0e5ff0c
Felipe Silva <feliperochasilva1987@gmail.com>
2019-06-21 21:52:44 -0300

    Exercise Switch Case

---
+++ Exercicio_Switch_Case/Exercicio_Switch_Case/Calculadora.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Switch_Case
+{
+    public class Calculadora
+    {
+        private double Numero1 { get; set; }
+
+        private double Numero2 { get; set; }
+
+        private string Operador { get; set; }
+
+
+        public double Somar(double numero1, double numero2)
+        {
+            this.Numero1 = numero1;
+            this.Numero2 = numero2;           
+
+            return this.Numero1 + this.Numero2;
+        }
+
+        public double Subtrair(double numero1, double numero2)
+        {
+            this.Numero1 = numero1;
+            this.Numero2 = numero2;            
+            
+            return this.Numero1 - this.Numero2;
+        }
+
+        public double Multiplicar(double numero1, double numero2)
+        {
+            this.Numero1 = numero1;
+            this.Numero2 = numero2;            
+            
+            return this.Numero1 * this.Numero2;
+        }
+
+        public double Dividir(double numero1, double numero2)
+        {
+            this.Numero1 = numero1;
+            this.Numero2 = numero2;            
+           
+            return this.Numero1 / this.Numero2;
+        }
+        
+        public void EscolherOperacao(double num1, string oper, double num2)
+        {          
+            switch (oper)
+            {
+                case "+":
+                    double soma = Somar(num1, num2);
+                    Console.WriteLine(soma);
+                    break;
+                case "-":
+                  double subtrair = Subtrair(num1, num2);
+                    Console.WriteLine(subtrair);
+                    break;
+                case "*":
+                    double multiplicar = Multiplicar(num1, num2);
+                    Console.WriteLine(multiplicar);
+                    break;
+                case "/":
+                    double dividir = Dividir(num1, num2);
+                    Console.WriteLine(dividir);
+                    break;
+                default:
+                    Console.WriteLine("OPERADOR NÃO FOI INICIADO!");
+                    break;
+            }            
+           
+        }
+
+    }
+}
---
+++ Exercicio_Switch_Case/Exercicio_Switch_Case/Processamento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Switch_Case
+{
+    public class Processamento
+    {
+        public void Processar()
+        {
+            Calculadora calculadora = new Calculadora();
+
+            Console.WriteLine("Entre com o primeiro numero: ");
+            double numero1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o operador desejado: ");
+            string operador = Console.ReadLine();            
+
+            Console.WriteLine("Entre com o segundo numero: ");
+            double numero2 = double.Parse(Console.ReadLine());
+
+            calculadora.EscolherOperacao(numero1, operador, numero2);          
+            
+
+        }        
+        
+
+    }
+}
