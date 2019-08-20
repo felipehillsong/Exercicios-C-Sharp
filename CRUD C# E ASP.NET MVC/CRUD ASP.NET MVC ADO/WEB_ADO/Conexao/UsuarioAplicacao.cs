9a46bc8d1e7e59bb17f8036b45276f6da763d82c
Felipe Silva <feliperochasilva1987@gmail.com>
2019-08-20 00:03:52 -0300

    continuando crudADO

--- CRUD C# E ASP.NET MVC/CRUD ASP.NET MVC ADO/WEB_ADO/Conexao/UsuarioAplicacao.cs
+++ CRUD C# E ASP.NET MVC/CRUD ASP.NET MVC ADO/WEB_ADO/Conexao/UsuarioAplicacao.cs
@@ -9,95 +9,34 @@ namespace WEB_ADO.Conexao
 {
     class UsuarioAplicacao
     {
-        private BD bd;
+        public UsuarioAplicacaoADO comando { get; private set; }
 
-        private void Insert(Usuarios usuarios)
+        public UsuarioAplicacao()
         {
-            var exeQuery = "";
-            exeQuery += "INSERT INTO usuarios(nome, cargo, data)";
-            exeQuery += string.Format(" VALUES('{0}','{1}','{2}')", usuarios.Nome, usuarios.Cargo, usuarios.Data);
-            using (bd = new BD())
-            {
-                bd.ExecutarComandoSemRetorno(exeQuery);
-            }
-        }
-
-        private void Update(Usuarios usuarios)
-        {
-            var exeQuery = "";
-            exeQuery += "UPDATE usuarios SET ";
-            exeQuery += string.Format("nome = '{0}',", usuarios.Nome);
-            exeQuery += string.Format("cargo = '{0}',", usuarios.Cargo);
-            exeQuery += string.Format("data = '{0}'", usuarios.Data);
-            exeQuery += string.Format("WHERE id = {0}", usuarios.Id);
-            using (bd = new BD())
-            {
-                bd.ExecutarComandoSemRetorno(exeQuery);
-            }
+            comando = new UsuarioAplicacaoADO();
         }
 
         public void Delete(int id)
         {
-            using (bd = new BD())
-            {
-                var exeQuery = string.Format("DELETE FROM usuarios WHERE id = {0}", id);
-                bd.ExecutarComandoSemRetorno(exeQuery);
-            }
+            comando.Delete(id);
         }
-
-        //metodo Salvar sera chamado para inserir ou deletar
+       
         public void Salvar(Usuarios usuarios)
         {
-            if (usuarios.Id > 0)
-            {
-                Update(usuarios);
-            }
-            else
-            {
-                Insert(usuarios);
-            }
+            comando.Salvar(usuarios);
         }
 
         public List<Usuarios> Select()
         {
-            using (bd = new BD())
-            {
-                var exeQuery = "SELECT * FROM usuarios";
-                var retorno = bd.ExecutaComandoComRetorno(exeQuery);
-                return LerDadosDoBanco(retorno);
-            }
+            return comando.Select();
 
         }
 
         public Usuarios SelectById(int id)
         {
-            using (bd = new BD())
-            {
-                var exeQuery = string.Format("SELECT * FROM usuarios WHERE id = {0}", id);
-                var retorno = bd.ExecutaComandoComRetorno(exeQuery);
-                return LerDadosDoBanco(retorno).FirstOrDefault();
-            }
+            return comando.SelectById(id);
 
         }
-
-        private List<Usuarios> LerDadosDoBanco(SqlDataReader reader)
-        {
-            var usuarios = new List<Usuarios>();
-            while (reader.Read())
-            {
-                var dados = new Usuarios()
-                {
-                    Id = int.Parse(reader["id"].ToString()),
-                    Nome = reader["nome"].ToString(),
-                    Cargo = reader["cargo"].ToString(),
-                    Data = DateTime.Parse(reader["data"].ToString())
-                };
-
-                usuarios.Add(dados);
-            }
-
-            reader.Close();
-            return usuarios;
-        }
+        
     }
 }
--- CRUD C# E ASP.NET MVC/CRUD ASP.NET MVC ADO/WEB_ADO/Models/Usuarios.cs
+++ CRUD C# E ASP.NET MVC/CRUD ASP.NET MVC ADO/WEB_ADO/Models/Usuarios.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +10,15 @@ namespace WEB_ADO.Models
     public class Usuarios
     {
         public int Id { get; set; }
-
+        [DisplayName("Nome")]
+        [Required(ErrorMessage = "Nome Obrigatório!")]
         public string Nome { get; set; }
-
+        [DisplayName("Cargo")]
+        [Required(ErrorMessage = "Cargo Obrigatório!")]
         public string Cargo { get; set; }
-
+        [DisplayName("Data de Cadastro")]
+        [Required(ErrorMessage = "Data Obrigatória!")]
+        [DisplayFormat(ApplyFormatInEditMode =true, DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime Data { get; set; }
     }
 }
