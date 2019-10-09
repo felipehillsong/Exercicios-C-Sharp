87c30db9986625ca8168bea7f03614a082d79edc
Felipe Silva <feliperochasilva1987@gmail.com>
2019-10-09 16:02:58 -0300

    finish the system 45

--- Exercicios de Logica/Estruturas de Repetição/PesquisaProduto/Processamento.cs
+++ Exercicios de Logica/Estruturas de Repetição/PesquisaProduto/Processamento.cs
@@ -15,10 +15,21 @@ namespace PesquisaProduto
             int idade;
             int sexo;
             int respostaSim = 0;
-            int respostaSimFinal = 0;
+            int respostaSimFinalHomem = 0;
+            int respostaNaoFinalHomem = 0;
+            int respostaSimFinalMulher = 0;
+            int respostaNaoFinalMulher = 0;
             int respostaNao = 0;
-            int respostaNaoFinal = 0;
-            int resposta;       
+            int resposta;
+            int numeroDeMulherNao = 0;
+            int numeroDeMulherSim = 0;
+            int totalDeMulheres = 0;
+            double porcentagemMulheresSim = 0;
+            int numeroDeHomemSim = 0;
+            int numeroDeHomemNao = 0;
+            double numerosRespostasSimMulheres = 0;
+            int totalDeVotosSim = 0;
+            int totalDeVotosNao = 0;
 
             Pessoa pessoa = new Pessoa();        
 
@@ -40,13 +51,15 @@ namespace PesquisaProduto
                     if(resposta == 1)
                     {
                         respostaSim = resposta;
-                        respostaSimFinal += 1;
+                        respostaSimFinalHomem += 1;
+                        numeroDeHomemSim += 1;
                         pessoa.Dados(nome, idade, sexo, respostaSim);
                     }
                     if (resposta == 2)
                     {
                         respostaNao = resposta;
-                        respostaNaoFinal += 1;
+                        respostaNaoFinalHomem += 1;
+                        numeroDeHomemNao += 1;
                         pessoa.Dados(nome, idade, sexo, respostaNao);
                     }
                 }
@@ -58,21 +71,33 @@ namespace PesquisaProduto
                     if (resposta == 1)
                     {
                         respostaSim = resposta;
-                        respostaSimFinal += 1;
+                        respostaSimFinalMulher += 1;
+                        numeroDeMulherSim += 1;
                         pessoa.Dados(nome, idade, sexo, respostaSim);
                     }
                     if (resposta == 2)
                     {
                         respostaNao = resposta;
-                        respostaNaoFinal += 1;
+                        respostaNaoFinalMulher += 1;
+                        numeroDeMulherNao += 1;
                         pessoa.Dados(nome, idade, sexo, respostaNao);
                     }
                 }
                 r++;
             }
 
-            Console.WriteLine(respostaSimFinal);
-            Console.WriteLine(respostaNaoFinal);
+            totalDeVotosSim = respostaSimFinalHomem + respostaSimFinalMulher;
+            totalDeVotosNao = respostaNaoFinalHomem + respostaNaoFinalMulher;
+
+            totalDeMulheres = numeroDeMulherSim + numeroDeMulherNao;
+            numerosRespostasSimMulheres = numeroDeMulherSim;
+            porcentagemMulheresSim = (numerosRespostasSimMulheres / totalDeMulheres) * 100;
+
+
+            Console.WriteLine("O numero de pessoas que responderam sim foram de: "+totalDeVotosSim);
+            Console.WriteLine("O numero de pessoas que responderam nãp foram de: "+totalDeVotosNao);
+            Console.WriteLine("O numero total de mulheres entrevistadas foram de: "+totalDeMulheres);
+            Console.WriteLine("A porcentagem de mulheres que votaram sim em relação ao total de mulheres foram de: "+ porcentagemMulheresSim);
         }
     }
 }
