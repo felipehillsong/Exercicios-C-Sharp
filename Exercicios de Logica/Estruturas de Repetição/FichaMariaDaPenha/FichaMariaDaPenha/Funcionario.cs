77bdc079c882e1021d3121b68d80998a24b81579
Felipe Silva <feliperochasilva1987@gmail.com>
2020-01-02 19:29:28 -0300

    Exercise50

---
+++ Exercicios de Logica/Estruturas de Repetição/FichaMariaDaPenha/FichaMariaDaPenha/Funcionario.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FichaMariaDaPenha
+{
+    public class Funcionario : IDados
+    {
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public int ClasseSalario { get; set; }
+        public double HorasTrabalhadas { get; set; }
+        public double HorasExtras { get; set; }
+        private double SalarioNormalFinal { get; set; }
+        private double SalarioHoraExtraFinal { get; set; }
+        private double SalarioComDesconto { get; set; }
+        private double SalarioFinalizado { get; set; }
+
+        public void Dados(Funcionario funcionario)
+        {
+            switch (funcionario.ClasseSalario)
+            {
+                case 1:
+                    if (funcionario.HorasExtras == 0)
+                    {
+                        this.SalarioNormalFinal = funcionario.HorasTrabalhadas * 16;
+                        this.SalarioComDesconto = (SalarioNormalFinal * 8) / 100;
+                        this.SalarioFinalizado = SalarioNormalFinal - SalarioComDesconto;
+                        Console.WriteLine($"\nNome do funcionario: {Nome}\nMatricula do funcionario {Nome}: {Matricula}\nHoras Trabalhadas: {HorasTrabalhadas}\nSalario Bruto: R${SalarioNormalFinal}\nHoras Extras: {HorasExtras}\nDesconto INSS: R${SalarioComDesconto}\nSalario Liquido: R${SalarioFinalizado}");
+                    }
+
+                    if (funcionario.HorasExtras > 0)
+                    {
+                        this.SalarioNormalFinal = funcionario.HorasTrabalhadas * 16;
+                        this.SalarioHoraExtraFinal = SalarioNormalFinal * 1.30;
+                        this.SalarioComDesconto = (SalarioHoraExtraFinal * 8) / 100;
+                        this.SalarioFinalizado = SalarioHoraExtraFinal - SalarioComDesconto;
+                        Console.WriteLine($"\nNome do funcionario: {Nome}\nMatricula do funcionario {Nome}: {Matricula}\nHoras Trabalhadas: {HorasTrabalhadas}\nSalario Bruto: R${SalarioNormalFinal}\nHoras Extras: {HorasExtras}\nSalario Bruto com horas extras: {SalarioHoraExtraFinal}\nDesconto INSS: R${SalarioComDesconto}\nSalario Liquido: R${SalarioFinalizado}");
+                    }
+                    break;
+                case 2:
+                    if (funcionario.HorasExtras == 0)
+                    {
+                        this.SalarioNormalFinal = funcionario.HorasTrabalhadas * 20;
+                        this.SalarioComDesconto = (SalarioNormalFinal * 8) / 100;
+                        this.SalarioFinalizado = SalarioNormalFinal - SalarioComDesconto;
+                        Console.WriteLine($"\nNome do funcionario: {Nome}\nMatricula do funcionario {Nome}: {Matricula}\nHoras Trabalhadas: {HorasTrabalhadas}\nSalario Bruto: R${SalarioNormalFinal}\nHoras Extras: {HorasExtras}\nDesconto INSS: R${SalarioComDesconto}\nSalario Liquido: R${SalarioFinalizado}");
+                    }
+
+                    if (funcionario.HorasExtras > 0)
+                    {
+                        this.SalarioNormalFinal = funcionario.HorasTrabalhadas * 20;
+                        this.SalarioHoraExtraFinal = SalarioNormalFinal * 1.30;
+                        this.SalarioComDesconto = (SalarioHoraExtraFinal * 8) / 100;
+                        this.SalarioFinalizado = SalarioHoraExtraFinal - SalarioComDesconto;
+                        Console.WriteLine($"\nNome do funcionario: {Nome}\nMatricula do funcionario {Nome}: {Matricula}\nHoras Trabalhadas: {HorasTrabalhadas}\nSalario Bruto: R${SalarioNormalFinal}\nHoras Extras: {HorasExtras}\nSalario Bruto com horas extras: {SalarioHoraExtraFinal}\nDesconto INSS: R${SalarioComDesconto}\nSalario Liquido: R${SalarioFinalizado}");
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Exercicios de Logica/Estruturas de Repetição/FichaMariaDaPenha/FichaMariaDaPenha/Processamento.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FichaMariaDaPenha
+{
+    public class Processamento
+    {
+        Funcionario funcionario { get; set; }
+
+        public Processamento()
+        {
+            funcionario = new Funcionario();
+        }
+
+        public void Processar()
+        {
+            int s = 0;
+
+            Console.WriteLine("Deseja calcular quantas fichas? ");
+            int fichas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o nome do funcionario: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.WriteLine($"Entre com a matricula do funcionario {funcionario.Nome}: ");
+            funcionario.Matricula = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Qual a classe de salario do funcionario {funcionario.Nome}? (1)R$16,00 por hora ou (2)R$20,00 por hora.");
+            funcionario.ClasseSalario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Entre com as horas trabalhadas do funcionario {funcionario.Nome}: ");
+            funcionario.HorasTrabalhadas = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Entre com as horas extras do funcionario {funcionario.Nome}: ");
+            funcionario.HorasExtras = double.Parse(Console.ReadLine());
+
+            funcionario.Dados(funcionario);
+
+            s = 1;
+
+            for (s = 2; s <= fichas; s++)
+            {
+                Console.WriteLine("\nEntre com o nome do funcionario: ");
+                funcionario.Nome = Console.ReadLine();
+
+                Console.WriteLine($"Entre com a matricula do funcionario {funcionario.Nome}: ");
+                funcionario.Matricula = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Qual a classe de salario do funcionario {funcionario.Nome}? (1)R$16,00 por hora ou (2)R$20,00 por hora.");
+                funcionario.ClasseSalario = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Entre com as horas trabalhadas do funcionario {funcionario.Nome}: ");
+                funcionario.HorasTrabalhadas = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Entre com as horas extras do funcionario {funcionario.Nome}: ");
+                funcionario.HorasExtras = double.Parse(Console.ReadLine());
+
+                funcionario.Dados(funcionario);
+            }
+
+        }
+    }
+}
