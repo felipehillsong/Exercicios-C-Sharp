844e4f3bee4f14e3094c4962d35ced311567650f
Felipe Silva <feliperochasilva1987@gmail.com>
2020-03-05 02:04:21 -0300

    exercise53

---
+++ Exercicios de Logica/Estruturas de Repetição/Exercicio53/Exercicio53/Disciplina.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio53
+{
+    public class Disciplina
+    {
+        public long Matricula { get; set; }
+
+        public double Nota1 { get; set; }
+
+        public double Nota2 { get; set; }
+
+        public double Nota3 { get; set; }
+
+        public double NotaFinal { get; set; }
+
+        public int Frequencia { get; set; }        
+
+        public void Dados(Disciplina disciplina)
+        {
+            disciplina.NotaFinal = (disciplina.Nota1 + disciplina.Nota2 + disciplina.Nota3) / 3;            
+
+            if (disciplina.NotaFinal >= 60 && disciplina.Frequencia >= 40)
+            {
+                Console.WriteLine($"O aluno com matricula {disciplina.Matricula} obteve as notas {disciplina.Nota1}, {disciplina.Nota2} e {disciplina.Nota3}, e oteve a média { disciplina.NotaFinal}. E sua frequencia foi de {disciplina.Frequencia}. Seu status é Aprovado");
+            }
+            else if(disciplina.NotaFinal < 60 || disciplina.Frequencia < 40)
+            {
+                Console.WriteLine($"O aluno com matricula {disciplina.Matricula} obteve as notas {disciplina.Nota1}, {disciplina.Nota2} e {disciplina.Nota3}, e oteve a média { disciplina.NotaFinal}. E sua frequencia foi de {disciplina.Frequencia}. Seu status é Reprovado");
+            }
+        }
+    }
+}
---
+++ Exercicios de Logica/Estruturas de Repetição/Exercicio53/Exercicio53/Processamento.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio53
+{
+    public class Processamento
+    {
+        public Disciplina disciplina { get; set; }
+
+        public Processamento()
+        {
+            disciplina = new Disciplina();
+        }
+
+        public void Processar()
+        {
+            double maiorNota = 0;
+            double menorNota = 0;
+            double notaFinal = 0;
+            int s = 0;
+
+            Console.WriteLine("Entre com a matricula do aluno: ");
+            disciplina.Matricula = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com a primeira nota do aluno: ");
+            disciplina.Nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com a segunda nota do aluno: ");
+            disciplina.Nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com a terceira nota do aluno: ");
+            disciplina.Nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com a frequencia do aluno: ");
+            disciplina.Frequencia = int.Parse(Console.ReadLine());
+
+            disciplina.Dados(disciplina);
+
+            notaFinal = (disciplina.Nota1 + disciplina.Nota2 + disciplina.Nota3) / 3;
+            maiorNota = notaFinal;
+            menorNota = notaFinal;
+
+            s = 1;
+
+            while (s < 3)
+            {
+                Console.WriteLine("Entre com a matricula do aluno: ");
+                disciplina.Matricula = long.Parse(Console.ReadLine());
+
+                Console.WriteLine("Entre com a primeira nota do aluno: ");
+                disciplina.Nota1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Entre com a segunda nota do aluno: ");
+                disciplina.Nota2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Entre com a terceira nota do aluno: ");
+                disciplina.Nota3 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Entre com a frequencia do aluno: ");
+                disciplina.Frequencia = int.Parse(Console.ReadLine());
+
+                disciplina.Dados(disciplina);
+
+                notaFinal = (disciplina.Nota1 + disciplina.Nota2 + disciplina.Nota3) / 3;
+
+                if (notaFinal < menorNota)
+                {
+                    menorNota = notaFinal;
+                }
+                else if(notaFinal > maiorNota)
+                {   
+                    maiorNota = notaFinal;
+                }
+
+                s++;
+            }
+
+            Console.WriteLine($"Maior Nota {maiorNota}, menor nota {menorNota}");
+        }
+    }
+}
