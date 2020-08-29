fae1616c2b6d61d63985a978f12a12f560af5d8c
Felipe Silva <feliperochasilva1987@gmail.com>
2020-08-29 00:15:20 -0300

    Teste Git no Visual Studio

--- Exemplo_Razor/Exemplo_Razor/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Exemplo_Razor.MvcApplication" Language="C#" %>
---
+++ Exercicios de Logica/Banco_City_Bank/ITeste.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco_City_Bank
+{
+    interface ITeste
+    {
+        string Imprimir();
+    }
+}
