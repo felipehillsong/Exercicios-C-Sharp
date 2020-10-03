b6225b63be4ed804c3ce49a6123395e359fe8cfb
Felipe Silva <feliperochasilva1987@gmail.com>
2020-10-02 23:58:02 -0300

    Testes em asp.core

---
+++ Primeiro Projeto ASP.CORE/API/TestesJson/TestesJson/Controllers/PessoaController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestesJson.Data;
+using TestesJson.Models;
+
+namespace TestesJson.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        private readonly MeuDbContext _context;
+
+        public PessoaController(MeuDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pessoa
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pessoa>>> GetPessoas()
+        {
+            return await _context.Pessoas.ToListAsync();
+        }
+
+        // GET: api/Pessoa/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pessoa>> GetPessoa(int id)
+        {
+            var pessoa = await _context.Pessoas.FindAsync(id);
+
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            return pessoa;
+        }
+
+        // PUT: api/Pessoa/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPessoa(int id, Pessoa pessoa)
+        {
+            if (id != pessoa.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pessoa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PessoaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pessoa
+        [HttpPost]
+        public async Task<ActionResult<Pessoa>> PostPessoa(Pessoa pessoa)
+        {
+            _context.Pessoas.Add(pessoa);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPessoa", new { id = pessoa.Id }, pessoa);
+        }
+
+        // DELETE: api/Pessoa/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Pessoa>> DeletePessoa(int id)
+        {
+            var pessoa = await _context.Pessoas.FindAsync(id);
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pessoas.Remove(pessoa);
+            await _context.SaveChangesAsync();
+
+            return pessoa;
+        }
+
+        private bool PessoaExists(int id)
+        {
+            return _context.Pessoas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Primeiro Projeto ASP.CORE/API/TestesJson/TestesJson/Data/MeuDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestesJson.Models;
+
+namespace TestesJson.Data
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Pessoa> Pessoas { get; set; }       
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                .Where(p => p.ClrType == typeof(string))))
+                property.Relational().ColumnType = "varchar(100)";
+
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
