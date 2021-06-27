3f9ea8378cd12da6c009d70a5e4ce124c98b625d
Felipe Silva <felipe.silva@vegatech.com.br>
2021-06-27 04:18:16 -0300

    continue course

--- ProjetoAgil/ProjetoAgilApp/src/app/Eventos/Eventos.component.ts
+++ ProjetoAgil/ProjetoAgilApp/src/app/Eventos/Eventos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Evento } from '../_models/Evento';
 import { EventoService } from '../_services/evento.service';
@@ -21,6 +22,7 @@ export class EventosComponent implements OnInit {
   imageMargem = 2;
   mostrarImagem = true;
   modalRef: BsModalRef;
+  registerForm: FormGroup;
   
   _filtroLista: string;  
   get filtroLista(): string{
@@ -46,7 +48,8 @@ export class EventosComponent implements OnInit {
   } 
 
   ngOnInit() {
-    this.getEventos();    
+    this.validation();  
+    this.getEventos();  
   }
 
   openModal(template: TemplateRef<any>){
@@ -66,6 +69,23 @@ export class EventosComponent implements OnInit {
 
   alternarImage(){
     this.mostrarImagem = !this.mostrarImagem;
+  }
+
+  validation(){
+    this.registerForm = new FormGroup({
+      tema: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
+      local: new FormControl('', Validators.required, ),
+      dataEvento: new FormControl('', Validators.required, ),
+      qtdPessoas: new FormControl('', [Validators.required, Validators.max(120000)]),
+      imageURL: new FormControl('', Validators.required, ),
+      telefone: new FormControl('', Validators.required, ),
+      email: new FormControl('', [Validators.required, Validators.email])
+    })
+
+  }
+
+  salvarAlteracao(){
+
   }
 
   filtrarEventos(filtrarPor: string): Evento[]{
