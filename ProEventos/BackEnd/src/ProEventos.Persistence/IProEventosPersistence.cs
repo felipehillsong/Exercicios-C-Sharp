3d99237b95fb6225814971e1fe799315066847c8
Felipe Silva <felipe.silva@vegatech.com.br>
2021-12-29 16:26:07 -0300

    Continue the course

---
+++ ProEventos/BackEnd/src/ProEventos.Persistence/IProEventosPersistence.cs
@@ -0,0 +1,29 @@
+﻿using ProEventos.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProEventos.Persistence
+{
+    public interface IProEventosPersistence
+    {
+        //GERAL
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        void DeleteRange<T>(T[] entity) where T : class;
+        Task<bool> SaveChangesAsync();
+
+        //EVENTOS
+        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
+        Task<Evento[]> GetEventoByIdAsync(int EventoId, bool includePalestrantes);
+        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes);
+
+        //PALESTRANTES
+        Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos);
+        Task<Palestrante[]> GetPalestranteByIdAsync(int PalestranteId, bool includeEventos);
+        Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string nome, bool includeEventos);
+    }
+}
