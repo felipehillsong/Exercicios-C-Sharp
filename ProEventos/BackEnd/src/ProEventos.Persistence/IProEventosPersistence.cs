9cc34b13b0f773ee557d1f97c2603127e1028b3c
Felipe Silva <felipe.silva@vegatech.com.br>
2021-12-30 19:38:48 -0300

    continue the course

--- ProEventos/BackEnd/src/ProEventos.Persistence/IProEventosPersistence.cs
+++ ProEventos/BackEnd/src/ProEventos.Persistence/IProEventosPersistence.cs
@@ -18,12 +18,12 @@ namespace ProEventos.Persistence
 
         //EVENTOS
         Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
-        Task<Evento[]> GetEventoByIdAsync(int EventoId, bool includePalestrantes);
+        Task<Evento> GetEventoByIdAsync(int EventoId, bool includePalestrantes);
         Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes);
 
         //PALESTRANTES
         Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos);
-        Task<Palestrante[]> GetPalestranteByIdAsync(int PalestranteId, bool includeEventos);
+        Task<Palestrante> GetPalestranteByIdAsync(int PalestranteId, bool includeEventos);
         Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string nome, bool includeEventos);
     }
 }
