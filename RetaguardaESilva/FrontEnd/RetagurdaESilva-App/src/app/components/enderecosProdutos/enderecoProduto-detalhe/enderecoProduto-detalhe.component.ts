9978934965d882297675ab15eac5dbc07ac068ca
Felipe Silva <feliperochasilva1987@gmail.com>
2023-01-23 19:25:25 -0300

    refatoração de alguns codigos do front

--- RetaguardaESilva/FrontEnd/RetagurdaESilva-App/src/app/components/enderecosProdutos/enderecoProduto-detalhe/enderecoProduto-detalhe.component.ts
+++ RetaguardaESilva/FrontEnd/RetagurdaESilva-App/src/app/components/enderecosProdutos/enderecoProduto-detalhe/enderecoProduto-detalhe.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-enderecoProduto-detalhe',
   templateUrl: './enderecoProduto-detalhe.component.html',
-  styleUrls: ['./enderecoProduto-detalhe.component.css']
+  styleUrls: ['./enderecoProduto-detalhe.component.scss']
 })
 export class EnderecoProdutoDetalheComponent implements OnInit {
 
--- RetaguardaESilva/FrontEnd/RetagurdaESilva-App/src/app/components/enderecosProdutos/enderecoProduto-editar/enderecoProduto-editar.component.ts
+++ RetaguardaESilva/FrontEnd/RetagurdaESilva-App/src/app/components/enderecosProdutos/enderecoProduto-editar/enderecoProduto-editar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-enderecoProduto-editar',
   templateUrl: './enderecoProduto-editar.component.html',
-  styleUrls: ['./enderecoProduto-editar.component.css']
+  styleUrls: ['./enderecoProduto-editar.component.scss']
 })
 export class EnderecoProdutoEditarComponent implements OnInit {
 
